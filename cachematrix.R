ee8bd0092b4de3f56c2949010cd1724442ea62cd
awood37 <alex7wood@gmail.com>
2016-06-17 14:18:32 -0600

    Alex Wood, wk3 assignment two

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera Week 3 assignment created by ALEX WOOD
 
-## Write a short comment describing this function
+## The first function will create a matrix object that
+## is able to cache its own inverse,
+## while the second function will compute the inverse of the same matrix
+## returned by makeCacheMatrix; but if the inverse has been
+## calculated, then the second function should get the inverse from the cache
+## that was created by the first function.
 
-makeCacheMatrix <- function(x = matrix()) {
 
+# the first function, which will create a matrix 
+# object that chaches its inverse.
+
+makeCacheMatrix <- function(x = matrix()){
+    invert <- NULL
+    set <- function(y) {
+        x <<- y
+        invert <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) invert <<- inverse
+    getInverse <- function() invert
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
+# This is the second function that will compute the inverse of the matrix
+# object created by the function above.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+    invert <- x$getInverse()
+    if(!is.null(invert)) {
+        message("getting cached data")
+        return(invert)
+    }
+    matrixI <- x$get()
+    invert <- solve(matrixI, ...)
+    x$setInverse(invert)
+    invert
+    # returns the inverse
 }
